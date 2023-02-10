ccf0c95a3a9a76082b2ff72b33646475b1bdf5bb
René Løwe Jacobsen <rlj@rloewe.net>
2023-02-10 15:30:11 +0100

    Fixes #6

--- lumi_allocations/cli.py
+++ lumi_allocations/cli.py
@@ -3,6 +3,7 @@
 from .data import ProjectInfo
 import argparse
 import os
+import grp
 
 
 def main():
@@ -16,21 +17,25 @@ def main():
         help="Project numbers comma seperated. Default: all of your projects",
         default="",
     )
-    parser.add_argument(
-        "-a",
-        "--all",
-        help="Return information for all projects.",
-        action="store_true",
-    )
-    parser.add_argument("--lust", action="store_true", help="Special flag for LUST")
+    parser.add_argument("-a", "--all", help=argparse.SUPPRESS, action="store_true")
+    parser.add_argument("--lust", help=argparse.SUPPRESS, action="store_true")
     args = parser.parse_args()
+    if args.lust and "project_462000008" not in [
+        grp.getgrgid(a).gr_name for a in os.getgroups()
+    ]:
+        parser.error("Error: You are not a LUST member")
+    if args.all and not args.lust:
+        parser.error("-a or --all can only be used with --lust")
     projdir = "/projappl"
     projects = []
     not_a_project = {"project_462000009": False, "project_465000002": False}
     if args.all is True:
         # get all projects through listing /projappl and filtering out non-existing projects
         for path in os.listdir(projdir):
-            if os.path.islink(os.path.join(projdir, path)) and path not in not_a_project:
+            if (
+                os.path.islink(os.path.join(projdir, path))
+                and path not in not_a_project
+            ):
                 projects.append(path)
         projects.sort()
     else:
